65b87b6670958dd31e392e93ecf686bec7a09773
John Tabash <jrtabash@yahoo.com>
2020-04-05 18:56:15 -0500

    Updated test epidemic script to accept model name and parameters command line arguments.

--- test_epidemic.py
+++ test_epidemic.py
@@ -1,28 +1,22 @@
 import math_models_epidemic as epidemic
 import sys
 
-def runSIR():
-    model = epidemic.SIRModel(transmitRate=3.5, removeRate=0.5)
+def runSIR(transmit=3.5, remove=0.5):
+    model = epidemic.SIRModel(transmitRate=transmit, removeRate=remove)
     t, sir = epidemic.solve(model, 15, 150)
     epidemic.plot(model, t, sir)
 
-def runSEIR():
-    model = epidemic.SEIRModel(transmitRate=3.5, infectRate=1.0, removeRate=0.5)
+def runSEIR(transmit=3.5, reducedEI=0.0, infect=1.0, remove=0.5):
+    model = epidemic.SEIRModel(transmitRate=transmit, reducedEIRate=reducedEI, infectRate=infect, removeRate=remove)
     t, seir = epidemic.solve(model, 20, 200)
     epidemic.plot(model, t, seir)
 
-def runSEIR2():
-    model = epidemic.SEIRModel(transmitRate=3.5, reducedEIRate=0.25, infectRate=1.0, removeRate=0.5)
-    t, seir = epidemic.solve(model, 20, 200)
-    epidemic.plot(model, t, seir)
-
-def run(which):
+def run(which, modelArgs):
     if which == 'sir':
-        runSIR()
+        runSIR(*modelArgs)
     elif which == 'seir':
-        runSEIR()
-    elif which == 'seir2':
-        runSEIR2()
+        runSEIR(*modelArgs)
 
 if __name__ == '__main__':
-    run(sys.argv[1] if len(sys.argv) > 1 else 'sir')
+    run(sys.argv[1] if len(sys.argv) > 1 else 'sir',
+        [float(arg) for arg in sys.argv[2:]] if len(sys.argv) > 2 else [])
